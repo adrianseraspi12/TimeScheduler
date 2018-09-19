353b371a369eb278ae99bb469954518a3f6d11e3
Adrian Seraspi <adrianseraspi12@gmail.com>
2018-09-19 16:40:44 +0800

    Added Splash Screen Feature

--- app/src/main/java/com/suzei/timescheduler/SplashActivity.java
+++ app/src/main/java/com/suzei/timescheduler/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.suzei.timescheduler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,7 +9,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
     }
 
 }
